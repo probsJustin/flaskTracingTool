e2615b24bae0ad94e929eda632dca52faebb75f3
Justin Hagerty <wheelsmanx@gmail.com>
2020-05-01 11:55:51 -0400

    rewrite and adding the standardization

--- flaskApp.py
+++ flaskApp.py
@@ -23,225 +23,100 @@ else:
 
 sdk = oneagent.get_sdk()
 
-def logRequest(requestFuncVar):
-    print(requestFuncVar.args)
-    logging.info(request.args)
+## constants
+SLRES = "X-dynaSupLabRes-info"
+SLREQ = "X-dynaSupLabReq-info"
+SLPATH = "X-dynaSupLabPath-info"
+SLPOS = "X-dynaSubLabPosition-info"
+SLURL = "url_passthrough"
+DYNASL = "Dynatrace Support Lab"
 
-def isDebug(requestFuncVar):
-    if(requestFuncVar.args.get("debug")):
-        print("Dynatrace SUPLAB Debug Variable is set to True")
-        if(requestFuncVar.args.get("debug") == "True" or requestFuncVar.args.get("debug") == "1"):
+responseHeaders = dict()
+returnBody = dict()
+
+def line(funcString = ""):
+    return funcString + '\n'
+
+def clearReturnBody():
+    global returnBody
+    returnBody = dict()
+
+def returnReturnBody():
+    global returnBody
+    return returnBody
+
+def addBody(funcStringVar):
+    global returnBody
+    try:
+        returnBody += funcStringVar
+    except:
+        returnBody = funcStringVar
+
+def appendToResHeaders(funcStringVar, funcIndexVar):
+    global responseHeaders
+    try:
+        responseHeaders[funcIndexVar] += funcStringVar
+    except:
+        responseHeaders[funcIndexVar] = funcStringVar
+
+def isDebug(funcRequest):
+    if(funcRequest.args.get("debug")):
+        if(funcRequest.args.get("debug") in ['true', 'True', '1']):
             return True
         else:
             return False
     else:
-        return False
+            return False
 
-def isPassThrough(requestFuncVar):
-    if(requestFuncVar.args.get("url_passthrough")):
-        print("Dynatrace SUPLAB Url Passthrough Variable is set.")
-        return True
+def checkForHeader(funcRequest, funcIndex):
+    returnObject = dict()
+    if(funcRequest.headers.get(funcIndex)):
+        returnObject["test"] = True
+        returnObject["unit"] = funcRequest.headers[funcIndex]
+        return returnObject
     else:
-        return False
+        returnObject["test"] = False
+        return returnObject
 
-
-def processRequest(requestFuncVar):
-    newResponse = dict()
-    newResponse['data'] = ""
-    newResponse['unit'] = False
-    if(isPassThrough(requestFuncVar)):
-        try:
-            newResponse['url_passthrough'] = requestFuncVar.args.get('url_passthrough')
-        except Exception as error:
-            newResponse['url_passthrough'] = False
-            print(error)
+def checkForArgs(funcRequest, funcIndex):
+    returnObject = dict()
+    if(funcRequest.args.get(funcIndex)):
+        returnObject["test"] = True
+        returnObject["unit"] = funcRequest.args.get(funcIndex)
+        return returnObject
     else:
-        newResponse['url_passthrough'] = False
-    if(isDebug(requestFuncVar)):
-        newResponse['unit'] = True
+        returnObject["test"] = False
+        return returnObject
 
-        newResponse['data'] = newResponse['data'] + "\n Dynatrace SUPLAB Debug Request URL \n"
-        newResponse['data'] = newResponse['data'] + "PATH : " + requestFuncVar.path + "\n"
-        newResponse['data'] = newResponse['data'] + "URL : " + requestFuncVar.url + "\n"
-
-        newResponse['data'] = newResponse['data'] + "Method : " + requestFuncVar.method + '\n'
-        newResponse['data'] = newResponse['data'] + "Remote Address : " + requestFuncVar.remote_addr + '\n'
-
-        newResponse['data'] = newResponse['data'] + "\n Dynatrace SUPLAB Debug Request Arguments: \n"
-        for x in requestFuncVar.args:
-            newResponse['data'] = newResponse['data'] + str(x) + " : " + requestFuncVar.args.get(x) + '\n'
-        newResponse['data'] = newResponse['data'] + "\n Dynatrace SUPLAB Debug Request Headers: \n"
-        for x in requestFuncVar.headers:
-            newResponse['data'] = newResponse['data'] + str(x) + '\n'
-        newResponse['data'] = newResponse['data'] + "\n Dynatrace SUPLAB Debug Cookies: \n"
-        for x in requestFuncVar.cookies:
-            newResponse['data'] = newResponse['data'] + str(x) + " : " + requestFuncVar.cookies.get(x) + '\n'
-    logRequest(requestFuncVar)
-    return newResponse
-
-def getCurrentAddress(headers):
-    return headers.args.get('X-dynaSupLabPath-info')
-
-
-def addRequestHeaderInfo(headers, requestIp, messageHeader, contentLength, requestURI):
-    newHeader = dict()
-    newHeader = headers
-    newHeader['X-dynaSupLabReq-info'] = newHeader['X-dynaSupLabReq-info'] + "ts=" + datetime.now() + ",client-ip=" + requestIp + ",message=" + messageHeader + ",content-length=" + contentLength + ",requestURI=" + requestURI + "$"
-    return newHeader
-
-def addRequestHeaderInfo(headers, requestIp, messageHeader, contentLength, requestURI):
-    newHeader = dict()
-    newHeader = headers
-    newHeader['X-dynaSupLabRes-info'] = newHeader['X-dynaSupLabRes-info'] + "ts=" + datetime.now() + ",client-ip=" + requestIp + ",message=" + messageHeader + ",content-length=" + contentLength + ",requestURI=" + requestURI + "$"
-    return newHeader
-
-def getNewHeaders(headers):
-    newHeader = dict()
-    newHeader['X-dynaSupLabPosition-info'] = str(headers['X-dynaSupLabPosition-info'])
-    newHeader['X-dynaSupLabPath-info'] = str(headers['X-dynaSupLabPath-info'])
-    if(newHeader['X-dynaSupLabPath-info'] == 'False'):
-        newHeader['X-dynaSupLabPosition-info'] = str(int(newHeader['X-dynaSupLabPosition-info']) + 1)
+def requestFactory(funcRequest):
+    clearReturnBody()
+    newRequest = funcRequest
+    if(isDebug(request)):
+        addBody(line(DYNASL + " Debug Output:") + line())
+        addBody(line(DYNASL + " Debug Header Output:"))
+        for x, y in newRequest.headers:
+            addBody(line(x + " : " + y))
+        addBody(line() + line(DYNASL + " Debug Header Flags Found:"))
+        if(checkForHeader(request, SLPATH)["test"] and checkForHeader(request, SLPOS)["test"]):
+            addBody(line(SLPATH + " : " + checkForHeader(request, SLPATH)["unit"]))
+            addBody(line(SLPOS + " : " + checkForHeader(request, SLPOS)["unit"]))
+        if(checkForHeader(request, SLREQ)["test"]):
+            addBody(line(SLREQ + " : " + checkForHeader(request, SLREQ)["unit"]))
+        if(checkForHeader(request, SLRES)["test"]):
+            addBody(line(SLRES + " : " + checkForHeader(request, SLRES)["unit"]))
+        addBody(line() + line(DYNASL + " Query Parameters Found:"))
+        if(checkForArgs(request, SLURL)["test"]):
+            addBody(line(SLURL + " : " + checkForArgs(request, SLURL)["unit"]))
+        if(checkForArgs(request, "html")["test"]):
+            addBody(line("HTML" + " : " + checkForArgs(request, "html")["unit"]))
     else:
-        newHeader['X-dynaSupLabPosition-info']
-    return newHeader
-
-
-def getNewPath(headers):
-    newHeader = dict()
-    newHeader['X-dynaSupLabPosition-info'] = str(headers['X-dynaSupLabPosition-info'])
-    newHeader['X-dynaSupLabPath-info'] = str(headers['X-dynaSupLabPath-info'])
-    splitNewHeader = str(newHeader['X-dynaSupLabPath-info']).split(',')
-    if(newHeader['X-dynaSupLabPosition-info'] == 'False'):
-        return splitNewHeader
-    else:
-        return splitNewHeader[int(newHeader['X-dynaSupLabPosition-info'])]
+        addBody("Debug is False")
+    return returnReturnBody()
 
 @autodynatrace.trace
 @app.route("/apiTest_GET", methods=['GET', 'POST', 'DELETE', 'PUT'])
 def apiTest_GET():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-    return responseObject
-
-@app.route("/apiTest_POST", methods=['GET', 'POST', 'DELETE', 'PUT'])
-def apiTest_POST():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-    return responseObject
-
-@app.route("/apiTest_DELETE", methods=['GET', 'POST', 'DELETE', 'PUT'])
-def apiTest_DELETE():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-    return responseObject
-
-@app.route("/apiTest_PUT", methods=['GET', 'POST', 'DELETE', 'PUT'])
-def apiTest_PUT():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-    return responseObject
-
-
-
-
-
-@app.route("/apiTest_CUSTOM", methods=['GET', 'POST', 'DELETE', 'PUT'])
-def apiTest_CUSTOM():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-    return responseObject
-
-
-
-
-
-@app.route("/apiTest_FAILURE", methods=['GET', 'POST', 'DELETE', 'PUT'])
-def apiTest_FAILURE():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-    abort(500)
-    return responseObject
-
-@app.route("/apiTest_SUCCESS", methods=['GET', 'POST'])
-def apiTest_SUCCESS():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-    return responseObject
-
-
-@app.route("/apiTest_PATH", methods=['GET', 'POST'])
-def apiTest_PATH():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        print(getCurrentAddress(request))
-        if(str(request.headers['X-dynaSupLabPosition-info']) == 'False'):
-            for x in getNewPath(request.headers):
-                response = requests.get(str(x))
-        else:
-            response = requests.get(getNewPath(request.headers), headers=getNewHeaders(request.headers))
-    return responseObject
-
-
-
-@app.route("/apiTest_responseCodeCheck", methods=['GET', 'POST', 'DELETE', 'PUT'])
-def apiTest_responseCodeCheck():
-    responseObject = Response("Hello World!")
-    processObject = processRequest(request)
-    if(processObject['unit']):
-        responseObject = Response(processObject['data'])
-        if(processObject['url_passthrough'] != False):
-            url = processObject['url_passthrough']
-            response = requests.get(url)
-
-    if(request.args.get('responseCode') and request.args.get('responseCode') != "200"):
-        print(str("Response: " + request.args.get('responseCode')))
-        abort(int(request.args.get('responseCode')))
-    else:
-        if(request.args.get('responseCode') == "200"):
-            print(str("Response: " + request.args.get('responseCode')))
-            print("Success - 200 received for response code checker")
-        else:
-            abort(500)
-            print("No Response Specified")
-    return responseObject
-
+    return requestFactory(request)
 
 if __name__ == "__main__":
     app.run(debug=True)
